92b962def4eaccfe5142b88e6f15570f2742043f
Curtis Sanderson <cw1sander@gmail.com>
2022-06-28 14:02:38 +0200

    updated shared models

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from shared_posts.models import Shared_Posts
-
 User = get_user_model()
 
 
@@ -14,11 +12,8 @@ class Post(models.Model):
     author = models.ForeignKey(to=User, on_delete=models.SET_NULL, blank=True, null=True, related_name='user_posts')
     liked_by = models.ManyToManyField(to=User, blank=True, related_name='liked_posts')
     is_liked = models.BooleanField(default=False)
-    # comments = models.ForeignKey(to=comment,blank=True,null=True, related_name='post_id') #will be changed later
-    embedding_post = models.OneToOneField(to=Shared_Posts, null=True, blank=True, related_name='parent_post')
-    is_shared_in = models.ForeignKey(to=Shared_Posts, blank=True, null=True, related_name='child_post')
 
+    # comments = models.ForeignKey(to=comment,blank=True,null=True, related_name='post_id') #will be changed later
 
     def __str__(self):
         return f'ID{self.id}: {self.posted_by}'
-
--- app/shared_posts/models.py
+++ app/shared_posts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from app.posts.models import Post
+
 
 class Shared_Posts(models.Model):
-    pass
+    parent_post = models.OneToOneField(to=Post, null=True, blank=True, related_name='embedding_post')
+    child_post = models.ForeignKey(to=Post, blank=True, null=True, related_name='is_shared_in')#possibly change
