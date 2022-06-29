bcd1702d3387dcdf1fac1c505a38f482266fc611
Curtis Sanderson <cw1sander@gmail.com>
2022-06-29 17:56:37 +0200

    added registration token view

--- app/registration/serializer.py
+++ app/registration/serializer.py
@@ -1,13 +1,23 @@
-from rest_framework import serializers
-from registration.models import Registration
-
-
-class RegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Registration
-        fields = '__all__'
-
-    class RegistrationCreateSerializer(RegistrationSerializer):
-        class Meta:
-            model = Registration
-            fields = '__all__'
+# from rest_framework import serializers
+# from registration.models import Registration
+# from users.serializers import UserSerializer
+#
+#
+# class RegistrationSerializer(serializers.ModelSerializer):
+#     profile = UserSerializer(read_only=True)
+#
+#     class Meta:
+#         model = Registration
+#         fields = ['email']
+#
+#
+# class RegistrationCreateSerializer(serializers.ModelSerializer):
+#     def save(self, validated_data):
+#         email = validated_data.get('email')
+#         registration = Registration(email=email)
+#         registration.save()
+#         return registration
+#     #
+#     # class Meta:
+#     #     model = Registration
+#     #     fields = ['code']
--- app/registration/views.py
+++ app/registration/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render
-from django.views import View
+from rest_framework.generics import GenericAPIView
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404
 import json
+from django.views import View
+from rest_framework.response import Response
 from registration.models import Registration
+from django.core.mail import send_mail
+
 
 
 class RegisterView(View):
+
     def post(self, request, *args, **kwargs):
-        data = json.loads(request.body)
-        new_registration = Registration.objects.create(**data)
-        new_registration.save()
-        content = {'email': new_registration.email, 'username': new_registration.username}
+        data = request.POST.get('email')
+        check_if_email_exists = []
+        for email in Registration.objects.all().values_list('email', flat=True):
+            check_if_email_exists.append(email)
+        if data in check_if_email_exists:
+            return JsonResponse('Email already provided', safe=False, status=200)
+        else:
+            new_user = Registration.objects.create(email=data)
+            new_user.save()
+            content = {'code': new_user.code}
+            send_mail(
+                "Thanks for Registering",
+                f'Here is your signup Token:{content}',
+                'djangod192@gmail.com',
+                [data],
+                fail_silently=False,
+              )
         return JsonResponse(content, status=201)
 
 
