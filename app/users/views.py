c521d65c58e815190d465b64e2e8e7007ab1b11c
S.Dharnia <s.dharnia@yahoo.com>
2022-06-29 16:18:17 +0200

    me added

--- app/users/views.py
+++ app/users/views.py
@@ -1,11 +1,14 @@
+import json
+
+from django.http import JsonResponse
 from rest_framework import status
-from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, get_object_or_404
 
 from rest_framework.response import Response
 
 from users.models import User
 from users.permissions import IsUseR
-from users.serializers import UserSerializer, UserCreateSerializer
+from users.serializers import UserSerializer, UserCreateSerializer, UserProfileSerializer
 
 
 class CreateUserView(ListCreateAPIView):
@@ -44,3 +47,35 @@ class FollowingView(GenericAPIView):
         queryset = User.objects.filter(followers__pk=request.user.pk)
         serializer = UserSerializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class UserProfileView(GenericAPIView):
+    permission_classes = [IsUseR]
+    serializer_class = UserProfileSerializer
+
+    def get(self, request, *args, **kwargs):
+        queryset = User.objects.filter(id=self.request.user.id)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class UserProfileUpdateView(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    lookup_url_kwarg = 'user_id'
+
+    # def patch(self, request, *args, **kwargs):
+    #     user_id = kwargs.get("user_id")
+    #     user = get_object_or_404(User, pk=user_id)
+    #     data = json.loads(request.body)
+    #     setattr(user, "username", data.get("username"))
+    #     user.save()
+    #     response = {
+    #         "id": user_id,
+    #         "username": user.username,
+    #
+    #     }
+    #     return JsonResponse(user_id)
+
+    # def perform_update(self, serializer):
+    #     serializer.save()
