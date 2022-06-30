37741a5110599ac29da99ab22312335befea2ac5
Curtis Sanderson <cw1sander@gmail.com>
2022-06-30 10:45:03 +0200

    updated validation view but still needs finishing

--- app/registration/urls.py
+++ app/registration/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from registration.views import RegisterView
+from registration.views import RegisterView, ValidationView
 
 urlpatterns = [
 
     path('registration/', RegisterView.as_view()),
-    # path('registration/validation/',),
+    # path('registration/validation/',ValidationView.as_view()),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_refresh'),
--- app/registration/views.py
+++ app/registration/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import get_object_or_404
 import json
 from django.views import View
 from rest_framework.response import Response
+
+import users.models
 from registration.models import Registration
 from django.core.mail import send_mail
-
 
 
 class RegisterView(View):
@@ -29,8 +30,40 @@ class RegisterView(View):
                 'djangod192@gmail.com',
                 [data],
                 fail_silently=False,
-              )
+            )
         return JsonResponse(content, status=201)
 
 
+class ValidationView(View):
+    def post(self, request, *args, **kwargs):
 
+        Email = request.POST.get('email')
+        code = request.POST.get('code')
+        username = request.POST.get('username')
+        firstname = request.POST.get('first_name')
+        lastname = request.POST.get('last_name')
+        password = request.POST.get('password')
+        passwordRepeat = request.POST.get('password_repeat')
+        database = Registration.objects.all().values_list('email', flat=True)
+        for email in database:
+            if Email in database:
+                whatever = Registration.objects.get(email=Email)
+                code2 = whatever.code
+                if code2 == code:
+                    database2 = users.models.User.objects.all().values_list('username', flat=True)
+                    for userNAme in database2:
+                        if username in database2:
+                            return JsonResponse('username taken please select another', status=201, safe=False)
+                        else:
+                            if password == passwordRepeat:
+
+                                new_user = users.models.User.objects.create(**data)
+                                new_user.save()
+                                return JsonResponse('password is the same', status=201, safe=False)
+                            else:
+                                return JsonResponse('passwords do not match', status=201, safe=False)
+                    return JsonResponse('it works2', status=201, safe=False)
+                else:
+                    return JsonResponse('it doesnt work1', status=201, safe=False)
+            else:
+                return JsonResponse('it doesnt work2', status=201, safe=False)
